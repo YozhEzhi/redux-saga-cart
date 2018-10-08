bdb28c2e304b6213fe107be57a7191806cde10aa
YozhEzhi <yozhezhi@gmail.com>
2018-10-09 00:42:44 +0300

    Первая сага

--- src/getStore.js
+++ src/getStore.js
@@ -5,33 +5,33 @@ import {
 } from 'redux';
 
 import { createLogger } from 'redux-logger';
-import { Iterable } from 'immutable'
-import thunk from 'redux-thunk'
+import { Iterable } from 'immutable';
+import thunk from 'redux-thunk';
 
-import { getQuery } from './utility'
+import { getQuery } from './utility';
 import { reducer } from './combineReducers';
-import { defaultState } from './defaultState'
+import { defaultState } from './defaultState';
+import createSagaMiddleware from 'redux-saga';
+import { initSagas } from './initSagas';
 
 const stateTransformer = (state) => {
     if (Iterable.isIterable(state)) return state.toJS();
     else return state;
 };
 
-const logger = createLogger({
-    stateTransformer,
-});
+const logger = createLogger({stateTransformer});
 
-export const getStore = ()=>{
-    const middleWares = [thunk];
+export const getStore = () => {
+    const sagaMiddleware = createSagaMiddleware();
+    const middleWares = [sagaMiddleware, thunk];
     if (getQuery()['logger']) { middleWares.push(logger)}
     const composables = [applyMiddleware(...middleWares)]
-    const enhancer = compose(
-        ... composables
-    );
+    const enhancer = compose(...composables);
     const store = createStore(
         reducer,
         defaultState,
         enhancer
     );
+    initSagas(sagaMiddleware);
     return store;
 };
